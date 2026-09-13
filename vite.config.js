bd51417a3ae744744b91681c86fb38207d548b23
nattarinmay <nattarinmay@gmail.com>
2026-09-13 06:00:54 +0000

    update

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-export default defineConfig({
-  root: ".",
-  appType: "spa",
-  server: { host: "0.0.0.0", port: 5500, strictPort: false },
-  build: { outDir: "dist", emptyOutDir: true }
-});
